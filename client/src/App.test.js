ba4cab22cc360510a8fbc0d2fa3844d247ab1630
irackson <nasirus@protonmail.com>
2021-08-16 23:06:07 -0400

    add test to enable CI=true

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -10,11 +10,11 @@ import App from "./App";
 //     console.log("THE FAIL TEST HAS ENDED");
 // });
 
-// test("test CI pipeline with pass", () => {
-//     // render(<App />);
-//     // const linkElement = screen.getByText(/Bart Yasso/i);
-//     // expect(linkElement).toBeInTheDocument();
-//     console.log("THE PASS TEST HAS BEGUN");
-//     expect(true).toBe(true);
-//     console.log("THE PASS TEST HAS ENDED");
-// });
+test("test CI pipeline with pass", () => {
+    // render(<App />);
+    // const linkElement = screen.getByText(/Bart Yasso/i);
+    // expect(linkElement).toBeInTheDocument();
+    console.log("THE PASS TEST HAS BEGUN");
+    expect(true).toBe(true);
+    console.log("THE PASS TEST HAS ENDED");
+});
